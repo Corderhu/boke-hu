e2efb807ad6fe612ec85f5cfa4c9418b762967a5
胡绪康 <1371468603@qq.com>
2022-03-07 13:35:30 +0800

    公司电脑修改

--- src/request/axios.js
+++ src/request/axios.js
@@ -1,16 +1,16 @@
-import config from '@/config/index.js'
-import axios from 'axios'
+import config from "@/config/index.js"
+import axios from "axios"
 
 const instance = axios.create({
-    baseurl:config.baseurl,
-    timeout:6000,
+    baseurl: config.baseurl,
+    timeout: 6000,
     headers: {
-        'content-type': "application/json"
-      }
+        "content-type": "application/json",
+    },
 })
 
 // 添加请求拦截器，主要用于token 认证问题
-instance.interceptors.request(request=>{
-
+instance.interceptors.request.use((request) => {
+    return request
 })
 console.log(config.baseurl)
