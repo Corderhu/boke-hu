8ba8f64b5f2af69716cb1f54a49aaacb9fbec4b0
李双双 <huxukangclub@163.com>
2022-03-07 17:40:34 +0800

    提交

--- src/request/axios.js
+++ src/request/axios.js
@@ -3,14 +3,24 @@ import axios from "axios"
 
 const instance = axios.create({
     baseurl: config.baseurl,
-    timeout: 6000,
+    timeout: 10000, // 10秒请求超时
     headers: {
         "content-type": "application/json",
     },
 })
 
 // 添加请求拦截器，主要用于token 认证问题
-instance.interceptors.request.use((request) => {
-    return request
+instance.interceptors.request.use((config) => {
+        let token = localStorage.getItem("token")
+        if (token) {
+            // 有token添加token ，无则不添加
+            config.headers["Authorization"] = "Bearer " + token
+        }
+        return config
+    })
+    //  处理请求结果
+
+instance.interceptors.response.use((config) => {
+    return config.data
 })
 console.log(config.baseurl)
