df8e8c8b1772481b6bb5ae5e4b5f9e0313c8557e
胡绪康 <1371468603@qq.com>
2022-03-08 23:07:33 +0800

    修改

--- src/router/pc/index.js
+++ src/router/pc/index.js
@@ -1,1 +1,10 @@
 // web端路由配置
+const pcRoute = [
+    {
+        path: "/pc_index/2", // pc端首页
+        name: "2",
+        component: () =>
+            import("@/views/pc_vue/2.vue"),
+    }
+]
+export default pcRoute
---
+++ src/views/pc_vue/2.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>web</div>
+</template>
