2559b8923b35849e01d4febb620440ad28cd82f8
李双双 <huxukangclub@163.com>
2022-03-08 09:08:19 +0800

    清除测试问题

--- src/request/axios.js
+++ src/request/axios.js
@@ -2,57 +2,64 @@ import config from "@/config/index.js"
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: config.baseurl,
-    timeout: 10000, // 10秒请求超时
-    headers: {
-        "content-type": "application/json",
-    },
+  baseURL: config.baseurl,
+  timeout: 10000, // 10秒请求超时
+  headers: {
+    "content-type": "application/json",
+  },
 })
 let refreshToken = false
 let newInstanceArr = []
 // 添加请求拦截器，主要用于token 认证问题
-instance.interceptors.request.use((config) => {
-    let token = localStorage.getItem("token")
-    if (token) {
-        // 有token添加token ，无则不添加
-        config.headers["Authorization"] = "Bearer " + token
-    }
-    return config
+instance.interceptors.request.use((request) => {
+  let token = localStorage.getItem("token")
+  if (token) {
+    // 有token添加token ，无则不添加
+    request.headers["Authorization"] = "Bearer " + token
+  }
+  return request
 })
 //  处理请求结果
 
-instance.interceptors.response.use((config) => {
-    let data = config.data
-    // return config.data
-    // 处理token 过期问题
-    if (data.code == 1001) {
-        if (!refreshToken) {
-            refreshToken = true
-            // 请求新的token
-            axios.get('http://192.168.1.11:8886/users/refreshToken', {params:{ token: data.data.token }}).then((data) => {
-                console.log(data)
-                if (data.data.code == 200) {
-                    config.headers['Authorization'] = data.data.data.token
-                    localStorage.setItem('token', data.data.data.token)
-                    newInstanceArr.forEach(element => {
-                        element()
-                    });
-                    newInstanceArr = []
-                }
-            }).catch(() => {
-
-            }).finally(() => {
-                refreshToken = false //重新归正，下次刷新
-            })
-        }
-        return new Promise(resolve=>{
-            newInstanceArr.push(()=>{
-                resolve(instance(config.config))
-            })
-           
+instance.interceptors.response.use((response) => {
+  let data = response.data
+  // return config.data
+  // 处理token 过期问题
+  if (data.code == 1001) {
+    if (!refreshToken) {
+      refreshToken = true
+      // 请求新的token
+      axios
+        .get("http://192.168.1.11:8886/users/refreshToken", {
+          params: { token: data.data.token },
         })
-        
+        .then((res) => {
+          if (res.data.code == 200) {
+            response.headers["Authorization"] = res.data.data.token
+            localStorage.setItem("token", res.data.data.token)
+            //请求完token
+            newInstanceArr.forEach((element) => {
+              element()
+            })
+            // 清空
+            newInstanceArr = []
+          }
+        })
+        .catch(() => {
+          // 重新登录
+        })
+        .finally(() => {
+          refreshToken = false //重新归正，下次刷新
+        })
     }
+    return new Promise((resolve) => {
+      newInstanceArr.push(() => {
+        resolve(instance(response.config))
+      })
+    })
+  } else {
+    return data
+  }
 })
-export default instance 
+export default instance
 // console.log(config.baseurl)
--- src/views/pc_vue/pc_index.vue
+++ src/views/pc_vue/pc_index.vue
@@ -2,27 +2,11 @@
   <div class="pc">web端 李双双是憨熊</div>
 </template>
 <script>
-import instance from "@/request/axios.js";
+import instance from "@/request/axios.js"
 
 export default {
-  created(){
-    // localStorage.clear()
-    this.getThings()
-  },
-  methods: {
-    getThings() {
-      instance({
-        url: "/users/first",
-        method: "get",
-        params: { token: localStorage.getItem("token") },
-      });
-      instance({
-        url: "/users/getThings",
-        methods: "get",
-        params: { token: localStorage.getItem("token") },
-      });
-    },
-  },
-};
+  created() {},
+  methods: {},
+}
 </script>
 <style scoped></style>
